494d6e7deb154a1c98f913f5fc153de41594ff6f
anurag-2420 <anuragpatel20june@gmail.com>
2024-06-10 15:38:44 +0530

    added build command

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 async function connect() {
     mongoose.set('strictQuery',true);
-    console.log(process.env.DB_URI);
     const db=await mongoose.connect(process.env.DB_URI);
     console.log("Database Connected Successfully");
     return db;
